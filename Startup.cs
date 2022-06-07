2fcc15092e21d244c966d64a3e66846729d78b60
Administrator <Administrator@DUCTAIPC>
2022-06-07 22:43:04 +0700

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LabBigSchool_DucTai.Startup))]
+namespace LabBigSchool_DucTai
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
